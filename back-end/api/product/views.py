604e5f189c24fff4c286822f1bad00977ab68b7f
Suzanne Loures <suzanneloures@gmail.com>
2021-02-06 19:59:50 -0300

    back-end: API com Verbos GET e POST funcionando para Product

--- back-end/api/product/views.py
+++ back-end/api/product/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from .models import Product
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import ProductSerializer
 
-# Create your views here.
+"""
+API endpoint que permite que produtos sejam visualizados ou editados.
+"""
+
+class ProductViewSet(viewsets.ModelViewSet):
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
